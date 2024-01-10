25fd2b8fddc0a5c696aba710731b59bf4eeeef1b
Kimberly Palmer <kimberlylpalmer@gmail.com>
2024-01-09 18:22:37 -0800

    Lab Code Along Complete

--- server/app.py
+++ server/app.py
@@ -9,19 +9,54 @@ from models import db, User, Review, Game
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-app.json.compact = False
+app.json.compact = False  # a configuration that has JSON responses print on separate lines with indentation
 
 migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/')
+
+@app.route("/")
 def index():
     return "Index for Game/Review/User API"
 
+
 # start building your API here
+@app.route("/games")
+def games():
+    games = [game.to_dict() for game in Game.query.all()]
+
+    response = make_response(
+        games, 
+        200
+    )
+    return response
 
 
-if __name__ == '__main__':
+@app.route("/games/<int:id>")
+def game_by_id(id):
+    game = Game.query.filter(Game.id == id).first()
+
+    game_dict = game.to_dict()
+
+    response = make_response(game_dict, 200)
+
+    return response
+
+@app.route('/games/users/<int:id>')
+def game_users_by_id(id):
+    game = Game.query.filter(Game.id == id).first()
+    users = [review.user.to_dict(rules=('-reviews',))
+             for review in game.reviews]
+    
+    response = make_response(
+        users,
+        200
+    )
+    
+    return response
+
+
+if __name__ == "__main__":
     app.run(port=5555, debug=True)
-
+    
